b51d6fe244eb2beef4a7c1d35d806937a3f69ffd
Basil Kohler <basil.kohler@gmail.com>
2011-09-23 11:49:41 +0200

    created hello world project

---
+++ darep/src/main/darep.java
@@ -0,0 +1,13 @@
+package main;
+
+public class darep {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Sick data repository you got there, bro");
+
+	}
+
+}
