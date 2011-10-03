b748d17383096b0b992aa3726eac8f2dd1bdc433
aeneaskaiser <aeneaskaiser@gmail.com>
2011-10-03 21:56:25 +0200

    formatted.  added commands

--- src/repos/Database.java
+++ src/repos/Database.java
@@ -7,16 +7,19 @@ import java.io.IOException;
 import java.nio.channels.FileChannel;
 
 /*representation of the physical database.
-*a database has two folders one for the datasets, located at 'filedb' 
-*and one for the metadatafiles, located at 'metadb'
-*/
+ *a database has two folders one for the datasets, located at 'filedb' 
+ *and one for the metadatafiles, located at 'metadb'
+ */
 public class Database {
 	File filedb;
 	File metadb;
 
-	/*loads(/creates if 'reposPath' doesnt exits) a db in 'reposPath'
-	 * doesnt open any files/folders just loads the paths
-	 * @param reposPath the location of the repository (parent folder od 'filedb' and 'metadb')
+	/*
+	 * loads(/creates if 'reposPath' doesnt exits) a db in 'reposPath' doesnt
+	 * open any files/folders just loads the paths
+	 * 
+	 * @param reposPath the location of the repository (parent folder od
+	 * 'filedb' and 'metadb')
 	 */
 	public Database(String reposPath) {
 		filedb = new File(reposPath + "/datasets");
@@ -28,38 +31,47 @@ public class Database {
 
 	}
 
-	/* if 'copyMode'=true, dataset is copied, else dataset is moved into the db
-	* does not check if 'name' is already used, this happens in Repository.add()
-	* @param dataset	the path to the dataset to be added
-	* @param meta		the metada corresponig to 'dataset'
-	* @param copyMode	true if dataset should be copied to the db, else dataset is moved into the db
-	*/
+	/*
+	 * if 'copyMode'=true, dataset is copied, else dataset is moved into the db
+	 * does not check if 'name' is already used, this happens in
+	 * Repository.add()
+	 * 
+	 * @param dataset the path to the dataset to be added
+	 * 
+	 * @param meta the metada corresponig to 'dataset'
+	 * 
+	 * @param copyMode true if dataset should be copied to the db, else dataset
+	 * is moved into the db
+	 */
 	public void add(File dataset, Metadata meta, boolean copyMode) {
 		String datasetPath = filedb.getAbsolutePath() + "/" + meta.getName();
 		String metadataPath = metadb.getAbsolutePath() + "/" + meta.getName();
+		
 		meta.saveAt(metadataPath);
+		
 		if (copyMode) {
 			FileChannel source = null;
 			FileChannel destination = null;
 			try {
 				dataset.createNewFile();
 				source = new FileInputStream(dataset).getChannel();
-				destination = new FileOutputStream(new File(datasetPath)).getChannel();
+				destination = new FileOutputStream(new File(datasetPath))
+						.getChannel();
 				destination.transferFrom(source, 0, source.size());
 				source.close();
 				source.close();
-
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-
 		} else {
 			dataset.renameTo(new File(datasetPath));
 		}
 	}
-	
-	/*return true if the db contains a dataset named 'name', else false
+
+	/*
+	 * return true if the db contains a dataset named 'name', else false
+	 * 
 	 * @ param name name of the file you are searching for
 	 */
 	public boolean contains(String name) {
--- src/repos/Repository.java
+++ src/repos/Repository.java
@@ -4,9 +4,8 @@ import java.io.File;
 
 import darep.Command;
 
-
 /*the repository provides methods to acces the Database.class
- * represents the physical folder located at 'location', which contains the db
+ * and represents the physical folder located at 'location', which contains the db
  * the repository ensures he correctness of the contents of the db !!!!
  * FINDET IHR DAS SINVOLL? 
  * (Repository.class KONTROLLIERT, Database.class SPEICHERT NUR WAS IHR GESAGT WIRD?)
@@ -14,76 +13,88 @@ import darep.Command;
 public class Repository {
 	private File location;
 	private Database db;
-	
-	/* loads(/creates) the default repo in user.home
-	 * 
+
+	/*
+	 * loads(/creates) the default (hidden) repo in user.home
 	 */
 	public Repository() {
-		location=new File(System.getProperty("user.home")+"/.data-repository");
+		location = new File(System.getProperty("user.home") + "/.data-repository");
 		if (!location.exists()) {
 			location.mkdir();
-			System.out.println("created new repository "+System.getProperty("user.home")+"/.data-reository");
+			System.out.println("created new repository "
+					+ System.getProperty("user.home") + "/.data-reository");
 		}
-		db=new Database(location.getAbsolutePath());
+		db = new Database(location.getAbsolutePath());
 	}
 
-	/* loads(/creates) the repo located at path
+	/*
+	 * loads(/creates) the repo located at path
 	 * 
-	 *  @param path where to find/create repo
+	 * @param path where to find/create repo
 	 */
 	public Repository(String path) {
-		location=new File(path);
+		location = new File(path);
 		if (!location.exists()) {
 			location.mkdir();
-			System.out.println("created new repository "+path);	
+			System.out.println("created new repository " + path);
 		}
-		db=new Database(location.getAbsolutePath());
+		db = new Database(location.getAbsolutePath());
 	}
 
-	/*adds a dataset to the db, as specified by the option in 'command'
-	 *  @param command the command object which stores the options
+	/*
+	 * adds a dataset to the db, as specified by the options in 'command'
+	 * 
+	 * @param command the command object which stores the options
 	 */
 	public void add(Command command) {
-		File dataset =new File(command.getParams()[0]);
+		File dataset = new File(command.getParams()[0]);
 		if (!dataset.exists()) {
-			System.out.println("file/folder does not exist.");
+			System.out.println("ERROR: file/folder does not exist.");
 			System.exit(1);
 		}
-		Metadata meta=makeMetadata(command);
-		db.add(dataset.getAbsoluteFile(),meta, !command.isSet("m"));
-		System.out.println("The file/folder "+meta.getOriginalName()+
-				" has been successfully added to the repository" +
-				" as data set named "+meta.getName());
+		
+		Metadata meta = makeMetadata(command);
+		
+		db.add(dataset.getAbsoluteFile(), meta, !command.isSet("m"));
+		
+		System.out.println("The file/folder " + meta.getOriginalName()
+				+ " has been successfully added to the repository"
+				+ " as data set named " + meta.getName());
 	}
 
+	/* creates a new valid metadata for a new dataset. 
+	 * "valid" means the returned Metadata is safe to be entered into the db.
+	 *@ param command  
+	 */
 	private Metadata makeMetadata(Command command) {
-		Metadata meta=new Metadata();
+		Metadata meta = new Metadata();
 		meta.setOriginalName(new File(command.getParams()[0]).getName());
+		
 		if (command.isSet("d"))
 			meta.setDescription(command.getOptionParam("d"));
+		
 		String name;
-		if (command.isSet("n")) {
-			name=command.getOptionParam("n");
-			if (db.contains(name)) {
-				System.out.println("ERROR: There is already a data set named data set name in the repository.");
+		if (command.isSet("n")) { //name option set?
+			name = command.getOptionParam("n");
+			if (db.contains(name)) { //if name exists, exit
+				System.out.println("ERROR: There is already a data set named "
+										+name+" name in the repository.");
 				System.exit(1);
-		}
-		}
-		else {
-			name=meta.getOriginalName().toUpperCase();
-			if (name.length()>40)
-				name=name.substring(0,39);
-		if (db.contains(name)) {
-			int append=1;
-		while (db.contains(name.concat(Integer.toString(append)))) {
-		append++;	
-		}
-		name=name.concat(Integer.toString(append));
-		}
+			}
+		} else { //no name provided, make a unique name from originalname
+			name = meta.getOriginalName().toUpperCase();
+			if (name.length() > 40)
+				name = name.substring(0, 39);
+			if (db.contains(name)) { //if name exists append number 
+				int append = 1;
+				while (db.contains(name.concat(Integer.toString(append)))) {
+					append++;
+				}
+				name = name.concat(Integer.toString(append));
+			}
 		}
 		meta.setName(name);
 		return meta;
-
 	}
 
 }
