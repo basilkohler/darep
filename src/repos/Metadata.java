0e2b349daad89e03f90b8135caf78e1fb76ef64b
Kevin Urban <kevin@Kevin-Urbans-MacBook-Pro.local>
2011-10-03 17:50:28 +0200

    deleted unnecessary imports in Metadata.java

--- src/repos/Metadata.java
+++ src/repos/Metadata.java
@@ -1,9 +1,7 @@
 package repos;
 
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
