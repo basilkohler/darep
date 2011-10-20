5f9b8a5b2dc3ec44adb91484a917c16dcccd3eaa
Kevin Urban <theonlyulti@gmail.com>
2011-10-20 14:29:05 +0200

    ParseException.getMessage() now has prefix "ERROR: "

--- src/darep/parser/ParseException.java
+++ src/darep/parser/ParseException.java
@@ -8,7 +8,7 @@ public class ParseException extends Exception {
 	private static final long serialVersionUID = 1L;
 
 	public ParseException(String string) {
-		super(string);
+		super("ERROR: " + string);
 	}
 	
 }
