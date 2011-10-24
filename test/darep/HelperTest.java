8a9568022b6fcd8c1ac326cf256194167c44adc3
Kevin Urban <theonlyulti@gmail.com>
2011-10-25 00:04:13 +0200

    HelperTest.testArrayIsPermutation

--- test/darep/HelperTest.java
+++ test/darep/HelperTest.java
@@ -17,5 +17,23 @@ public class HelperTest {
 			assertFalse(Helper.arrayContains(element, test1));
 		}
 	}
+	
+	@Test
+	public void testArrayIsPermutation() {
+		String[] test1 = new String[] {"this", "is", "a", "test"};
+		String[] permutation = new String[] {"a", "this", "test", "is"};
+		String[] toFew = new String[] {"test", "this", "is"};
+		String[] toMany = new String[] {"test", "is", "PWN3D!", "this", "a"};
+		String[] notPermutation = new String[] {"haha", "not", "same"};
+		String[] empty = new String[0];
+		String[] theSame = test1.clone();
+		
+		assertTrue(Helper.arrayIsPermutation(test1, permutation));
+		assertTrue(Helper.arrayIsPermutation(test1, theSame));
+		assertFalse(Helper.arrayIsPermutation(test1, toFew));
+		assertFalse(Helper.arrayIsPermutation(test1, toMany));
+		assertFalse(Helper.arrayIsPermutation(test1, notPermutation));
+		assertFalse(Helper.arrayIsPermutation(test1, empty));
+	}
 
 }
