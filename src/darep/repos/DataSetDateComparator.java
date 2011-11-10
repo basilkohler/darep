0fbd54e8b16f4b1727575210bf371f2162c8d1ff
Kevin Urban <theonlyulti@gmail.com>
2011-11-10 17:16:41 +0100

    added DataSetDateComparator

---
+++ src/darep/repos/DataSetDateComparator.java
@@ -0,0 +1,19 @@
+package darep.repos;
+
+import java.util.Comparator;
+import java.util.Date;
+
+/**
+ * This class can be user to sort DataSets by Date
+ */
+public class DataSetDateComparator implements Comparator<DataSet> {
+
+	@Override
+	public int compare(DataSet dataSet1, DataSet dataSet2) {
+		Date d1 = dataSet1.getMetadata().getTimeStamp();
+		Date d2 = dataSet2.getMetadata().getTimeStamp();
+		
+		return (int)(d1.getTime() - d2.getTime());
+	}
+
+}
