531517a4e1d40fb27bddd61f7149d027f8d3fffe
Basil Kohler <basil.kohler@gmail.com>
2011-11-16 11:59:02 +0100

    finished basic implementation of Server (untested)

--- src/darep/server/Server.java
+++ src/darep/server/Server.java
@@ -1,17 +1,21 @@
 package darep.server;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
 import darep.Command;
+import darep.Command.ActionType;
 import darep.repos.Repository;
+import darep.repos.RepositoryException;
 
 public class Server {
 	
 	private Repository repository;
 	private Properties properties = new Properties();
+	private Command userCommand;
 	private static final String[] propertyNames = {"incoming-directory", "html-overview", "log-file", 
 											"checking-interval-in-seconds", "completeness-checker.class-name" };
 	
@@ -24,13 +28,21 @@ public class Server {
 	private boolean running = true;
 	public Server(Repository repository, Command command) throws ServerException {
 		this.repository = repository;
+		this.userCommand = command;
 		loadPropertiesFile(command.getParams()[0]);
 	}
 	
 	public void start() throws ServerException {
 		while(running) {
-			
-			int seconds = Integer.parseInt(getPropertiesValue(CHECKING_INTERVAL_IN_SECONDS));
+			File[] files = checkIncomingDirectory();
+			for(File f : files) {
+				try {
+					addFile(f); 
+				} catch (RepositoryException e) {
+					throw new ServerException("server could not add file " + f.getPath());
+				}
+			}
+			int seconds = Integer.parseInt(getProperty(CHECKING_INTERVAL_IN_SECONDS));
 			try {
 				Thread.sleep(seconds*1000);
 			} catch (InterruptedException e) {
@@ -39,7 +51,7 @@ public class Server {
 		}
 	}
 	
-	private String getPropertiesValue(int propertyKey) {
+	private String getProperty(int propertyKey) {
 		return properties.getProperty(propertyNames[propertyKey]);
 	}
 	
@@ -58,4 +70,14 @@ public class Server {
 		}
 	}
 	
+	private File[] checkIncomingDirectory() {
+		File dir = new File(getProperty(INCOMING_DIRECTORY));
+		// TODO implement completeness-checker
+		return dir.listFiles();
+	}
+	
+	private void addFile(File file) throws RepositoryException {
+		Command command = new Command(ActionType.add, new String[] {file.getPath()}, null, new String[] {"m"});
+		this.repository.add(command);
+	}
 }
