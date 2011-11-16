7af01185eeba91be5189da3a2e6c753fa4eea27f
Basil Kohler <basil.kohler@gmail.com>
2011-11-16 11:43:14 +0100

    implemented basic structure for Server.start()

--- src/darep/server/Server.java
+++ src/darep/server/Server.java
@@ -12,17 +12,35 @@ public class Server {
 	
 	private Repository repository;
 	private Properties properties = new Properties();
-	private static String[] propertyNames = {"incoming-directory", "html-overview", "log-file", 
+	private static final String[] propertyNames = {"incoming-directory", "html-overview", "log-file", 
 											"checking-interval-in-seconds", "completeness-checker.class-name" };
 	
+	private static final int INCOMING_DIRECTORY = 0;
+	private static final int HTML_OVERVIEW = 1;
+	private static final int LOG_FILE = 2;
+	private static final int CHECKING_INTERVAL_IN_SECONDS = 3;
+	private static final int COMPLETENESS_CHECKER_CLASS_NAME = 4;
 	
+	private boolean running = true;
 	public Server(Repository repository, Command command) throws ServerException {
 		this.repository = repository;
 		loadPropertiesFile(command.getParams()[0]);
 	}
 	
 	public void start() throws ServerException {
-		
+		while(running) {
+			
+			int seconds = Integer.parseInt(getPropertiesValue(CHECKING_INTERVAL_IN_SECONDS));
+			try {
+				Thread.sleep(seconds*1000);
+			} catch (InterruptedException e) {
+				throw new ServerException(e);
+			}
+		}
+	}
+	
+	private String getPropertiesValue(int propertyKey) {
+		return properties.getProperty(propertyNames[propertyKey]);
 	}
 	
 	private void loadPropertiesFile(String propertiesPath) throws ServerException {
