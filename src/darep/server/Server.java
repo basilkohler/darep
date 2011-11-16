269ae0af91868aef472b2867279095cd2038e636
Basil Kohler <basil.kohler@gmail.com>
2011-11-16 11:15:07 +0100

    improved loadPropertiesFile

--- src/darep/server/Server.java
+++ src/darep/server/Server.java
@@ -12,11 +12,9 @@ public class Server {
 	
 	private Repository repository;
 	private Properties properties = new Properties();
-	private String incomingDirectory;
-	private String htmlOverview;
-	private String logFile;
-	private String checkingIntervalInSeconds;
-	private String completenessCheckerClassName;
+	private static String[] propertyNames = {"incoming-directory", "html-overview", "log-file", 
+											"checking-interval-in-seconds", "completeness-checker.class-name" };
+	
 	
 	public Server(Repository repository, Command command) throws ServerException {
 		this.repository = repository;
@@ -30,12 +28,11 @@ public class Server {
 	private void loadPropertiesFile(String propertiesPath) throws ServerException {
 		try {
 			properties.load(new FileInputStream(propertiesPath));
-			incomingDirectory = properties.getProperty("incoming-directory");
-			htmlOverview = properties.getProperty("html-overview");
-			logFile = properties.getProperty("log-file");
-			checkingIntervalInSeconds = properties.getProperty("checking-interval-in-seconds");
-			completenessCheckerClassName = properties.getProperty("completeness-checker.class-name");
-			
+			for(String p : propertyNames) {
+				if(properties.getProperty(p) == null) {
+					throw new ServerException("Missing property " + p + " in the property file " + propertiesPath);
+				}
+			}
 		} catch (FileNotFoundException e) {
 			throw new ServerException("could not find properties file " + propertiesPath, e);
 		} catch (IOException e) {
