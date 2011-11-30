99f4b6208fdcaa0a0ca95a713b0e178fdb8589b7
Kevin Urban <theonlyulti@gmail.com>
2011-11-30 14:06:03 +0100

    implemented MarkerFileChecker

--- plugins/darep/plugins/MarkerFileChecker.java
+++ plugins/darep/plugins/MarkerFileChecker.java
@@ -10,9 +10,26 @@ public class MarkerFileChecker implements CompletenessChecker {
 	private String prefix;
 
 	@Override
-	public File[] getCompletedFiles(File directory) {
-		File[] files = directory.listFiles();
+	public File[] getCompletedFiles(File directory) throws Exception {
 		ArrayList<File> complete = new ArrayList<File>();
+		File[] files = directory.listFiles();
+		
+		for (File file: files) {
+			String fileName = file.getName();
+			if (fileName.startsWith(prefix)) {
+				String newFileName = fileName.substring(prefix.length());
+				File newFile = new File(directory, newFileName);
+				if (!newFile.exists()) {
+					throw new Exception(
+							"Could not find matching file" +
+							" for lockfile '" + fileName + "'");
+				}
+				
+				complete.add(newFile);
+			}
+		}
+		
+		return complete.toArray(new File[0]);
 	}
 
 	@Override
