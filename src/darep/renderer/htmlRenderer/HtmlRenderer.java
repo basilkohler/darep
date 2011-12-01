7372fffba95b4ef17665ac1d86fff21096c5b67e
Basil Kohler <basil.kohler@gmail.com>
2011-12-01 12:03:03 +0100

    htmlrenderer creates a string with a html representation of the repo

--- src/darep/renderer/htmlRenderer/HtmlRenderer.java
+++ src/darep/renderer/htmlRenderer/HtmlRenderer.java
@@ -1,7 +1,12 @@
 package darep.renderer.htmlRenderer;
 
+import java.util.Arrays;
+
+import darep.Helper;
 import darep.renderer.Renderer;
 import darep.storage.DataSet;
+import darep.storage.DataSetDateComparator;
+import darep.storage.Metadata;
 
 public class HtmlRenderer implements Renderer {
 
@@ -14,12 +19,41 @@ public class HtmlRenderer implements Renderer {
 		html.append("<title>data-repository html list</title>");
 		html.append("<meta charset=\"utf-8\">");
 		html.append("</head><body>");
+		
+		html.append("<h1>data-repository</h1>");
 	
-		// TODO insert html list here
-		html.append("awesome list is awesome");
+		Arrays.sort(dataSets, new DataSetDateComparator());
+		int totalSize = 0;
+		
+		html.append("<table>");
+		html.append("<tr>");
+		for (String s: Metadata.columnHeaders) {
+			html.append("<td>");
+			html.append(s);
+			html.append("</td>");
+		}
+		html.append("</tr>");			
+		for (DataSet ds : dataSets) {
+			Metadata meta = ds.getMetadata();
+			totalSize += meta.getFileSize();
+			String[] values = meta.toStringArray();
+			
+			html.append("<tr>");
+			for(String s:values) {
+				html.append("<td>");
+				html.append(s);
+				html.append("</td>");
+			}
+			html.append("</tr>");
+		}
+		html.append("</table>");
+		
+		html.append("<p>");
+		html.append("(" + dataSets.length + " data sets, ");
+		html.append(totalSize + " bytes in total)");
+		html.append("</p>");
 		
 		html.append("</body></html>");
 		return html.toString();
 	}
-
 }
